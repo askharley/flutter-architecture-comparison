7cb39708c61f2f5af1d0a35e562af2e888769ccd
Harley Ferguson <harleyferguson@Harleys-MacBook-Pro.local>
2019-05-29 23:19:19 +0200

    Removed BaseView and BaseModel from Scoped Model architecture.

--- lib/src/architectures/scoped_model/scoped_models/posts_model.dart
+++ lib/src/architectures/scoped_model/scoped_models/posts_model.dart
@@ -1,22 +1,25 @@
+import 'package:scoped_model/scoped_model.dart';
+import 'package:flutter_architecture_comparison/src/architectures/provider/enums/enums.dart';
 import 'package:flutter_architecture_comparison/src/core/locator.dart';
 import 'package:flutter_architecture_comparison/src/core/models/post.dart';
 import 'package:flutter_architecture_comparison/src/core/services/repository.dart';
-import 'package:flutter_architecture_comparison/src/architectures/scoped_model/scoped_models/base_model.dart';
 
-class PostsScopedModel extends BaseModel {
+class PostsScopedModel extends Model {
   Repository _repository = locator<Repository>();
-
+  ViewState _state;
   bool _isLoading = false;
   List<Post> _posts;
 
+  ViewState get state => _state;
   bool get isLoading => _isLoading;
   List<Post> get posts => _posts;
 
-  int getCount() {
-    return _posts.length;
+  void setState(ViewState newState) {
+    _state = newState;
+    notifyListeners();
   }
 
-  Future<List<Post>> getPosts() async {
+  void getPosts() async {
     _isLoading = true;
     _posts = await _repository.getPosts();
     _isLoading = false;
